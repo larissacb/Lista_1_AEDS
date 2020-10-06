3626b4e3f1e9d15f4314679a352a2d831d32a1c4
Larissa de Castro Braga <71854519+larissacb@users.noreply.github.com>
2020-10-05 21:48:03 -0300

    Add files via upload

---
+++ Tarefa.py
@@ -0,0 +1,68 @@
+﻿class tarefa :
+
+    def __init__(self,nome,materia,dia,mes,ano,ponto):
+        self.nome = nome
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+        self.ponto = ponto
+        self.materia = materia
+
+
+    def setNome(self,nome):
+        self.nome=nome
+
+    def getNome(self):
+        return self.nome
+
+    def setDia(self,dia):
+        self.dia =dia
+
+    def getDia(self):
+        return self.dia
+
+    def setMes(self,mes):
+        self.mes=mes
+
+    def getMes(self):
+        return self.mes
+
+    def setAno(self,ano):
+        self.ano=ano
+
+    def getAno(self):
+        return self.ano
+
+    def setPonto(self,ponto):
+        self.ponto=ponto
+
+    def getPonto(self):
+        return self.ponto
+
+    def setMateria(self,materia):
+        self.materia=materia
+
+    def getMateria(self):
+        return self.materia
+
+    # def adicionar(self,nome,materia,dia,mes,ano,ponto):
+    #     self.nome = nome
+    #     self.dia = dia
+    #     self.mes = mes
+    #     self.ano = ano
+    #     self.ponto = ponto
+    #     self.materia = materia
+
+    # def excluir(self):
+    #     self.setNome == null
+    #     self.setMateria == null
+    #     self.setPonto == null
+    #     self.setDia == null
+    #     self.setMes == null
+    #     self.setAno == null
+
+    def imprimir(self):
+        print('Titulo :',self.nome)
+        print('Materia :',self.materia)
+        print('Pontuacao :',self.ponto)
+        print('Data :',self.dia,'/',self.mes,'/',self.ano)
---
+++ main.py
@@ -0,0 +1,40 @@
+﻿from Tarefa import tarefa
+
+y = True
+
+
+tarefas=list()
+while y==True: #loop
+            print('Menu')
+            print('1 - Adicionar Tarefa')
+            print('2 - Excluir Tarefa')
+            print('3 - Informacoes da Tarefa')
+            print('0 - Sair')
+            x =int(input('\nDigite um numero :' ))
+
+
+            if x == 1:
+                a=tarefa(input('Titulo :'),input('materia :'),int(input('dia :')),int(input('mes :')),int(input('ano :')),float(input('pontuacao :'))) #tarefa é uma classe
+                tarefas.append(a) #tarefas é uma lista
+                #tarefa[j].adicionar(input('Titulo :'),input('materia :'),input('dia :'),input('mes :'),input('ano :'),input('pontuacao :'))
+                #j+=1
+                #pass
+            elif x == 2:
+                k = int(input('Digite o numero da Tarefa')) #tenho que converter o numero para inteiro pq ele fica salvo como string
+                k-=1 #considero que o usuario digita o numero da tarefa ignorando a posicao 0
+                lixo= tarefas.pop(k) #apago o elemento da posicao k da lista. mas esse metodo retorna o valor excluido, coisa que nao e importante. por isso salvo em lixo
+                # tarefa[k-1].excluir(input('Digite o titulo da tarefa :'))
+                # k = 1000
+                # pass
+            elif x == 3:
+                k = int(input('Digite o numero da Tarefa'))  # tenho que converter o numero para inteiro pq ele fica salvo como string
+                k -= 1  # considero que o usuario digita o numero da tarefa ignorando a posicao 0
+                b = tarefas[k] #b e um objeto
+                b.imprimir() #utilizo o metodo imprimir
+                # l = 1000
+                # pass #isso nao existe. o pass e usado para outra coisa
+            elif x==0: break
+            
+            # print(k)
+            # print(l)
+
