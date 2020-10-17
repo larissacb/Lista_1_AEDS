cf7b64d40762953e29e2414f245ed1d0fef61b5f
Larissa de Castro Braga <71854519+larissacb@users.noreply.github.com>
2020-10-17 18:38:44 -0300

    Update Fila.py

--- Fila.py
+++ Fila.py
@@ -3,17 +3,22 @@ class fila:
     def __init__(self):
         self.items = list()
     
-    def addFila(self, item):
+    def enfileira(self, item): #Insere o item no final da lista
         self.items.append(item)
         
-    def removeFila(self):
+    def desenfileira(self): #Retorna o item do inicio da fila, retirando-o da lista
         return self.items.pop()
 
-    def getPosicao(self, posicao):
+    def getPosicao(self, posicao): #Retorna o item de uma determinada posicao
         return self.items[posicao]
     
-    def getTamanho(self):
+    def getTamanho(self): #Retorna o tamanho da lista
         return len(self.items)
     
-    def apagar(self):
+    def FFVazia(self): #Faz a fila ficar vazia
         self.items.clear()
+        
+    def Vazia(self): #Retorna true se a fila esta vazia; senao retorna false
+        if(self.getTamanho() == 0): return True
+        else: return False
+   
