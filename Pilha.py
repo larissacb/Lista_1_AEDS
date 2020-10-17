ffcdf8912524e0b8cfded6e5a729cded104b04a2
Larissa de Castro Braga <71854519+larissacb@users.noreply.github.com>
2020-10-17 18:37:12 -0300

    Update Pilha.py

--- Pilha.py
+++ Pilha.py
@@ -3,17 +3,21 @@ class pilha:
     def __init__(self):
         self.items = list()
         
-    def addPilha(self, item):
+    def empilha(self, item): #Insere o item no topo da pilha
         self.items.insert(0,item)
     
-    def removePilha(self):
+    def desempilha(self): #Retorna o item no topo da pilha, retirando-o da pilha
         return self.items.pop(0)
 
-    def getPosicao(self, posicao):
+    def getPosicao(self, posicao): #Retorna o item de uma determinada posicao
         return self.items[posicao]
     
-    def getTamanho(self):
+    def getTamanho(self): #Retorna o numero de itens da pilha
         return len(self.items)
     
-    def apagar(self):
+    def FPVazia(self): #Faz a pilha ficar vazia
         self.items.clear()
+        
+    def Vazia(self): #Retorna true se a pilha esta vazia; senao retorna false
+        if(self.getTamanho() == 0): return True
+        else: return False
